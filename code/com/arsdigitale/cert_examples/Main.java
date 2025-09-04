1609f3727215f9544aab40081129d35fe91ec9ef
Romito Anacoreta <romitoanacoreta@google.com>
2025-09-04 18:44:07 +0200

    Updating data types and index

--- code/com/arsdigitale/cert_examples/Main.java
+++ code/com/arsdigitale/cert_examples/Main.java
@@ -4,10 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Float.MIN_VALUE: " + Float.MIN_VALUE);
 		
-		int CLASS = 3;
-		int Class = 5;
 	}
 
 }
